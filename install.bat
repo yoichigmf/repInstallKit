bee63752cd1498b8f9ba3bc563a3118d2c931c8e
yoichigmf <yoichi.kayama@gmail.com>
2020-11-30 17:46:13 +0900

    first commit

---
+++ install.bat
@@ -0,0 +1,6 @@
+py3_env
+pip install --no-deps et_xmlfile-1.0.1.tar.gz
+pip install --no-deps jdcal-1.4.1.tar.gz
+pip install --no-deps openpyxl-3.0.5.tar.gz
+pip install --no-deps openpyxl-image-loader-1.0.5.tar.gz
+
